fd9e04854e52a98095ec554c15d167e110a61bff
Tahmid Azam <tahmid_azam@icloud.com>
2025-08-06 14:24:40 -0400

    fix: update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,6 +3,8 @@ import type { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "EBC Intranet",
+    short_name: "EBC",
+    description: "EBC Intranet",
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
