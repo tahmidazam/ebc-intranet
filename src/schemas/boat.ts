529f3cf784f62b30d98a653a8f670cd96adfa38c
Tahmid Azam <tahmid_azam@icloud.com>
2025-10-24 15:07:14 +0100

    feat: update boats

--- src/schemas/boat.ts
+++ src/schemas/boat.ts
@@ -1,6 +1,12 @@
 export const BOATS = [
-  { name: "Phil Turner (VIII)", configuration: "8+" },
-  { name: "Spirit (VIII)", configuration: "8+" },
-  { name: "Paul Collins (IV)", configuration: "4+" },
-  { name: "Rev Reed (IV)", configuration: "4+" },
+  { name: "Phil Turner (Men's 8+)", configuration: "8+" },
+  { name: "Spirit (Men's 8+)", configuration: "8+" },
+  { name: "Paul Collins (Men's 4+)", configuration: "4+" },
+  { name: "Rev Reed (Men's 4+)", configuration: "4+" },
+  { name: "John Horner (Women's 8+)", configuration: "8+" },
+  { name: "Dame Fi (Women's 8+)", configuration: "8+" },
+  { name: "P&M (Women's 8+)", configuration: "8+" },
+  { name: "Sprague (Women's 4+)", configuration: "4+" },
+  { name: "Lord Wilson (Women's 8+)", configuration: "8+" },
+  { name: "EBCA (Women's 4+)", configuration: "4+" },
 ];
