b3e389e194424b17f3911bc97b391492e5d58590
Tahmid Azam <tahmid_azam@icloud.com>
2025-10-27 12:55:52 +0000

    feat: add preview for events

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-import { sessionsToICalEventData } from "@/lib/sessions-to-ical-event-data";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { abbreviateName } from "@/lib/abbreviate-name";
+import { sessionsToResolvedEvents } from "@/lib/sessions-to-events";
+import { cn } from "@/lib/utils";
+import { Event } from "@/schemas/event";
+import { SEAT_LABELS } from "@/schemas/seat";
 import { useQuery } from "convex/react";
 import { format, getDay, parse, startOfWeek } from "date-fns";
+import { formatInTimeZone } from "date-fns-tz";
 import { enGB } from "date-fns/locale";
-import { Loader2Icon } from "lucide-react";
-import { SetStateAction, useMemo, useState } from "react";
+import { CircleCheck, CircleQuestionMark, Loader2Icon } from "lucide-react";
+import Link from "next/link";
+import React, { SetStateAction, useMemo, useState } from "react";
 import { Calendar, dateFnsLocalizer, View, Views } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { api } from "../../../../convex/_generated/api";
@@ -27,11 +42,11 @@ export default function AdminCalendar() {
   const collections = useQuery(api.collections.get);
 
   const events = useMemo(() => {
-    return sessionsToICalEventData({
-      sessions: sessions || [],
-      users: users || [],
-      collections: collections || [],
-    });
+    return sessionsToResolvedEvents(
+      sessions || [],
+      users || [],
+      collections || []
+    );
   }, [sessions, users, collections]);
 
   const [view, setView] = useState<View>(Views.MONTH);
@@ -44,7 +59,7 @@ export default function AdminCalendar() {
     setView(newView);
   };
 
-  console.log(events);
+  const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
 
   if (!sessions || !users || !collections)
     return (
@@ -66,8 +81,133 @@ export default function AdminCalendar() {
         onNavigate={handleNavigate}
         titleAccessor="summary"
         popup
-        style={{ height: "100%", width: "100%" }}
+        onSelectEvent={setSelectedEvent}
+        className="w-full h-full p-4"
       />
+
+      <Sheet
+        open={selectedEvent !== null}
+        onOpenChange={() => setSelectedEvent(null)}
+      >
+        <SheetContent>
+          {selectedEvent && (
+            <>
+              <SheetHeader>
+                <SheetTitle>{`${formatInTimeZone(
+                  selectedEvent.start,
+                  "Europe/London",
+                  "HH:mm"
+                )}–${formatInTimeZone(
+                  selectedEvent.end,
+                  "Europe/London",
+                  "HH:mm"
+                )} (${selectedEvent.duration}min)`}</SheetTitle>
+                <SheetDescription>{selectedEvent.summary}</SheetDescription>
+              </SheetHeader>
+              <div key={selectedEvent.id} className="flex flex-col px-4 gap-2">
+                <div className="grid grid-cols-2 gap-4 text-sm">
+                  <div className="flex flex-col gap-1">
+                    {selectedEvent.boat && (
+                      <div>
+                        <p className="text-muted-foreground">Boat</p>
+                        <p>{selectedEvent.boat}</p>
+                      </div>
+                    )}
+
+                    {selectedEvent.coachName && (
+                      <div>
+                        <p className="text-muted-foreground">Coach</p>
+                        <p>{selectedEvent.coachName}</p>
+                      </div>
+                    )}
+
+                    {selectedEvent.course && selectedEvent.distance && (
+                      <div>
+                        <p className="text-muted-foreground">
+                          Course & Distance
+                        </p>
+                        <p>{`${selectedEvent.course} (${selectedEvent.distance}km)`}</p>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="grid grid-cols-[auto_1fr_auto] gap-x-2 auto-rows-min items-center">
+                    {selectedEvent.seats &&
+                      SEAT_LABELS.map((seatLabel) => {
+                        const name = selectedEvent.seats[seatLabel]?.name;
+                        const id = selectedEvent.seats[seatLabel]?.id;
+                        if (!name || !id) return null;
+                        return (
+                          <React.Fragment key={seatLabel}>
+                            <p
+                              className={cn(
+                                "text-muted-foreground",
+                                selectedEvent.userSeat === seatLabel && "italic"
+                              )}
+                            >
+                              {seatLabel}
+                            </p>
+                            <p
+                              className={cn(
+                                selectedEvent.userSeat === seatLabel &&
+                                  "font-medium italic"
+                              )}
+                            >
+                              {abbreviateName(name)}
+                            </p>
+
+                            {selectedEvent.read.includes(id) ? (
+                              <CircleCheck className="size-4 text-green-500" />
+                            ) : (
+                              <CircleQuestionMark className="size-4 text-muted-foreground" />
+                            )}
+                          </React.Fragment>
+                        );
+                      })}
+                  </div>
+                </div>
+
+                {selectedEvent.outline && selectedEvent.outline != "" && (
+                  <div className="text-sm">
+                    <span className="text-muted-foreground">Outline</span>
+                    <p>{selectedEvent.outline}</p>
+                  </div>
+                )}
+              </div>
+
+              <SheetFooter>
+                {collections.find(
+                  (v) => v.title === selectedEvent.collectionTitle
+                ) && (
+                  <Button variant="outline" className="rounded-full">
+                    <Link
+                      href={`http://localhost:3000/admin/collections/${
+                        collections.find(
+                          (v) => v.title === selectedEvent.collectionTitle
+                        )?._id
+                      }/sessions`}
+                    >
+                      Go to sessions for {selectedEvent.collectionTitle}
+                    </Link>
+                  </Button>
+                )}
+
+                <Button className="rounded-full">
+                  <Link
+                    href={`http://localhost:3000/admin/collections/${
+                      collections.find(
+                        (v) => v.title === selectedEvent.collectionTitle
+                      )?._id
+                    }/sessions/${selectedEvent.id}/edit`}
+                  >
+                    Edit Session
+                  </Link>
+                </Button>
+              </SheetFooter>
+            </>
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   );
 }
--- src/schemas/event.ts
+++ src/schemas/event.ts
@@ -1,23 +1,26 @@
 import { z } from "zod";
 
 export const eventSchema = z.object({
-    id: z.string(),
-    summary: z.string(),
-    duration: z.number(),
-    start: z.date(),
-    end: z.date(),
-    collectionTitle: z.string(),
-    type: z.string(),
-    boat: z.string().optional(),
-    course: z.string().optional(),
-    distance: z.number().optional(),
-    coachName: z.string().optional(),
-    outline: z.string().optional(),
-    seats: z.record(z.string(), z.object({
-        name: z.string(),
-        id: z.string(),
-    })),
-    userSeat: z.string().nullable().optional(),
-    read: z.array(z.string()),
+  id: z.string(),
+  summary: z.string(),
+  duration: z.number(),
+  start: z.date(),
+  end: z.date(),
+  collectionTitle: z.string(),
+  type: z.string(),
+  boat: z.string().optional(),
+  course: z.string().optional(),
+  distance: z.number().optional(),
+  coachName: z.string().optional(),
+  outline: z.string().optional(),
+  seats: z.record(
+    z.string(),
+    z.object({
+      name: z.string(),
+      id: z.string(),
+    })
+  ),
+  userSeat: z.string().nullable().optional(),
+  read: z.array(z.string()),
 });
 export type Event = z.infer<typeof eventSchema>;
